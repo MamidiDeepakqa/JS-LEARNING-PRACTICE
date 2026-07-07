4a28eb10bbae0f23dff8f250ec178b90fd56a16a
Deepak Mamidi <mamidideepakqa@gmail.com>
2026-07-07 23:20:04 +0530

    some object tests

--- class6_objects.js
+++ class6_objects.js
@@ -11,3 +11,35 @@ let user ={
 //Accessing value...
 
 console.log(user.name);
+
+let key = "name";
+
+console.log((user[key]));
+
+console.log(user)
+
+//Updating the value in object...
+
+user.name = "QA Engineer";
+
+console.log(user.name);
+
+console.log(user)
+
+//Adding a new property...
+
+user.experience = 10;
+
+console.log(user);
+
+//Get all the values of an object without knowing the key's
+let users = {
+  name: "Deepak",
+  role: "QA Engineer",
+  isLoggedIn: true
+};
+
+Object.keys(users).forEach((key)=>{
+
+    console.log(key + " : " + users[key]);
+});
