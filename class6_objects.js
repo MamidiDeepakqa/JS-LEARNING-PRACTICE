e69b32f7d830daacdac5518915a5ef7fae8ff955
Deepak Mamidi <mamidideepakqa@gmail.com>
2026-07-07 19:04:08 +0530

    create new branch with all learning of JS

---
+++ class6_objects.js
@@ -0,0 +1,13 @@
+//Objects........
+
+let user ={
+    name : "test user",
+    role : "qa",
+    location : "hyd"
+}
+
+//Object is a collection of key-value pair.
+
+//Accessing value...
+
+console.log(user.name);
