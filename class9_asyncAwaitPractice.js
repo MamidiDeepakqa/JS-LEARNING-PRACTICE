643ee51230f4fb6b9f5dd2d96ee1dee75ac5f0e7
Deepak Mamidi <mamidideepakqa@gmail.com>
2026-07-20 15:05:02 +0530

    Added new class9 for async await practice

---
+++ class9_asyncAwaitPractice.js
@@ -0,0 +1,27 @@
+
+async function getDetails() {
+
+    const response = await fetch("https://restful-booker.herokuapp.com/booking/4");
+
+    if (response.status === 200) {
+
+        const convertedResponse = await response.json();
+
+        console.log(convertedResponse);
+
+        console.log("firstname : ", convertedResponse.firstname);
+        console.log("lastname : ", convertedResponse.lastname);
+        console.log("checkIn Date : ", convertedResponse.bookingdates.checkin);
+
+        if (convertedResponse.depositpaid === true) {
+            console.log("Deposit confirmed");
+        } else {
+            console.log("Deposit pending");
+        }
+    } else {
+        console.log("API failed : " + response.status);
+    }
+
+}
+
+getDetails();
